17db166e26cec36170fe76abb2a79c115d3d9043
Teoh Jie Yie <jieyiet2003@gmail.com>
2024-08-24 19:26:37 +0800

    first commit

---
+++ app/src/main/java/com/jy/mob23quizappfirebase/MyApp.kt
@@ -0,0 +1,9 @@
+package com.jy.mob23quizappfirebase
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp: Application() {
+
+}
---
+++ app/src/main/java/com/jy/mob23quizappfirebase/core/Constants.kt
@@ -0,0 +1,4 @@
+package com.jy.mob23quizappfirebase.core
+
+object Constants {
+}
